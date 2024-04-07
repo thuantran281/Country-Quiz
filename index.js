76de514587bcc00c4095b328c177522afa729103
Thuan Tran <tranthuan12002@gmail.com>
2024-04-07 14:35:15 +0300

    test

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const quizScreen = document.getElementById("screen");
 
 let data = [];
 let question = 0,
-  score = 0, choice = 4;
-let capitalCity, city, flag, country, check;
+  score = 0,
+  choice = 4;
+let capitalCity, cities, flag, country, check, previousAns;
 
 fetch("https://restcountries.com/v3.1/all?fields=name,capital,flag")
   .then((response) => {
@@ -23,15 +24,6 @@ function clearScreen() {
   quizScreen.textContent = "";
 }
 
-function country() {
-  clearScreen();
-  question += 1;
-}
-
-function flag() {
-
-}
-
 function capitalCities() {
   clearScreen();
   question += 1;
@@ -39,31 +31,57 @@ function capitalCities() {
   capitalCity = Math.floor(Math.random() * data.length);
 
   if (data.length === 0 || data[capitalCity].capital === "") {
-    city = [undefined]; 
+    cities = [undefined];
     check = undefined;
   } else {
-    city = [data[capitalCity].capital];
+    cities = [data[capitalCity].capital];
     check = data[capitalCity].capital;
   }
 
-  // initialise the city into an array so it stores the answers from the user
-
-  while (city.length < choice) {
+  while (cities.length < choice) {
     let options = data[Math.floor(Math.random() * data.length)].capital;
 
-    if (!city.includes(options) || !city.includes(undefined)) {
+    if (!cities.includes(options) || !cities.includes(undefined)) {
       if (options === "") {
-        city.push(undefined);
+        cities.push(undefined);
       } else {
-        city.push(options);
+        cities.push(options);
       }
     }
   }
 
-  quizScreen.textContent += 
-  `
-  <h2>Question ${question}</h2>
-  <h2>What is the capital city of ${data[capitalCity].name}?</h2><br />
-  
-  `
+  const answerBtn = document.getElementById("answers");
+  answerBtn.textContent = "";
+
+  quizScreen.textContent += `
+    <h2>Question ${question}</h2>
+    <h2>What is the capital city of ${data[capitalCity].name}?</h2><br />
+    <button type="submit" id="answers"></button>
+  `;
+
+  cities.map((city) => {
+    const cityBtn = document.createElement("button");
+    cityBtn.textContent = city;
+    cityBtn.onclick = isCapitalCorrect.bind(null, city);
+    answerBtn.appendChild(cityBtn);
+  });
+}
+
+function isCapitalCorrect(ans) {
+  clearScreen();
+
+  if (ans === check) {
+    score += 1;
+    capitalCities();
+  } else {
+    previousAns.textContent += `
+      Wrong answer :(\n
+      The capital city of ${data[capitalCity].name} is ${check} 
+    `;
+  }
+}
+
+function countries() {
+  clearScreen();
+  question += 1;
 }
